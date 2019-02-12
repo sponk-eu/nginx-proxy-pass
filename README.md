f062f974f57c74cd9131eb164375a7da2c93693f
Maciej Pochroń <m.pochron@stermedia.pl>
2019-02-12 15:38:23 +0100

    update

--- README.md
+++ README.md
@@ -25,3 +25,15 @@ services:
 ```
 docker run -e TARGET_SERVER_ENDPOINT='http://app:8000\/api\/' -e TARGET_SERVER='app:8000' -e PROXY_SSL_VERIFY='off' --name nginx-proxy-pass sponk/nginx-proxy-pass
 ```
+
+* Environments List
+
+| Environment            | Required | Default value |
+|------------------------|----------|---------------|
+| TARGET_SERVER_ENDPOINT | YES      |               |
+| HOST_SERVER            | NO       | $host         |
+| PROXY_SSL_VERIFY       | NO       | on            |
+| PROXY_CONNECT_TIMEOUT  | NO       | 60            |
+| PROXY_SEND_TIMEOUT     | NO       | 60            |
+| PROXY_READ_TIMEOUT     | NO       | 60            |
+| PROXY_BUFFERS          | NO       | 4 32k         |
--- start.sh
+++ start.sh
@@ -2,9 +2,17 @@
 
 HOST_SERVER=${HOST_SERVER:-\$host}
 PROXY_SSL_VERIFY=${PROXY_SSL_VERIFY:-on}
+PROXY_CONNECT_TIMEOUT=${PROXY_CONNECT_TIMEOUT:-60}
+PROXY_SEND_TIMEOUT=${PROXY_SEND_TIMEOUT:-60}
+PROXY_READ_TIMEOUT=${PROXY_READ_TIMEOUT:-60}
+PROXY_BUFFERS=${PROXY_BUFFERS:-4 32k}
 
 /bin/sed -i "s@<proxy_pass_placeholder>@${TARGET_SERVER_ENDPOINT}@" /etc/nginx/conf.d/default.conf
 /bin/sed -i "s@<host_placeholder>@${HOST_SERVER}@" /etc/nginx/conf.d/default.conf
 /bin/sed -i "s@<proxy_ssl_verify>@${PROXY_SSL_VERIFY}@" /etc/nginx/conf.d/default.conf
+/bin/sed -i "s@<proxy_connect_timeout>@${PROXY_CONNECT_TIMEOUT}@" /etc/nginx/conf.d/default.conf
+/bin/sed -i "s@<proxy_send_timeout>@${PROXY_SEND_TIMEOUT}@" /etc/nginx/conf.d/default.conf
+/bin/sed -i "s@<proxy_read_timeout>@${PROXY_READ_TIMEOUT}@" /etc/nginx/conf.d/default.conf
+/bin/sed -i "s@<proxy_buffers>@${PROXY_BUFFERS}@" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
